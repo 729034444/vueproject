148becee7abbe85230ef8478bbff81fdc415d463
729034444@qq.com <729034444@qq.com>
2020-03-12 18:30:20 +0800

    0312

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
+    <div id="app">
+        <home></home>
+        <router-view></router-view>
+        <main-tab-bar></main-tab-bar>
 
+    </div>
 </template>
 
 <script>
+import MainTabBar from './components/content/mainTabBar/MainTabBar'
+import Home from './views/home/Home'
 export default {
-    
+    name: 'app',
+    components:{
+        MainTabBar,
+        Home
+    }
 }
 </script>
 
 
 <style>
-    @import 'assets/css/base.css'
+    @import 'assets/css/base.css';
+    
 </style>
 
